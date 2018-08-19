cb6cfb9976607ea750ee3f21d1636d7d7d74db3e
mv04 <mtvickery1@gmail.com>
2018-08-19 17:20:16 -0500

    recreated portfolio with materialize

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".parallax").parallax();
+    $('.sidenav').sidenav();
+});
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
